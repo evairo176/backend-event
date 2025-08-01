d55463914596ed6942c61cea29edbfad124a0da4
Dicki Prasetya <dickiprasetya176@gmail.com>
2025-08-01 10:23:07 +0700

    feat: added mfa login controller

--- dist/cummon/validators/auth.validator.js
+++ dist/cummon/validators/auth.validator.js
@@ -30,7 +30,7 @@ exports.loginSchema = zod_1.z.object({
     identifier: zod_1.z.string().trim().min(1).max(50).or(exports.emailSchema),
     password: exports.passwordSchema,
     userAgent: zod_1.z.string().trim().optional(),
-    code: zod_1.z.number().optional(),
+    code: zod_1.z.string().optional(),
 });
 exports.verificationEmailSchema = zod_1.z.object({
     code: exports.verificationCodeSchema,
